0633229612995eaf2c4592887e70639ab4872528
sandy98 <esavoretti@gmail.com>
2022-08-03 00:10:15 +0000

    adding built-in templates ability

--- bicchiere.py
+++ bicchiere.py
@@ -615,15 +615,90 @@ class TemplateLight:
             render_context.update(context)
         return self._render_function(render_context, self._do_dots)
 
+header_prefix_html = """
+<!DOCTYPE html>
+  <html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+"""
+
+header_suffix_html = """
+    <title> {{ page_title }} </title>
+  </head>
+"""
+
+font_awesome_style = """
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+"""
 body_style = """
-<style>
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-</style>
+  <style>
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  </style>
 """
 menu_style = """
+  <style>
+  .navbar {
+    overflow: hidden;
+    background-color: #676767;
+  }
 
+  .navbar a {
+    float: left;
+    font-size: 16px;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  .dropdown {
+    float: left;
+    overflow: hidden;
+  }
+
+  .dropdown .dropbtn {
+    font-size: 16px;
+    border: none;
+    outline: none;
+    color: white;
+    padding: 14px 16px;
+    background-color: inherit;
+    font-family: inherit;
+    margin: 0;
+  }
+
+  .navbar a:hover, .dropdown:hover .dropbtn {
+    background-color: steelblue;
+  }
+
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+  }
+
+  .dropdown-content a {
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+  }
+
+  .dropdown-content a:hover {
+    background-color: #ddd;
+  }
+
+  .dropdown:hover .dropdown-content {
+    display: block;
+  }
+  </style>
 """
 
 class MenuBuilder:
