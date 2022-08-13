d6c37b1ead87020cdbf01353d79794ebba7ed29f
sandy98 <esavoretti@gmail.com>
2022-08-13 05:23:08 +0000

    version 0.2.4

--- bicchiere.py
+++ bicchiere.py
@@ -916,7 +916,7 @@ class Bicchiere(BicchiereMiddleware):
     Main WSGI application class
     """
 
-    __version__ = (0, 2, 3)
+    __version__ = (0, 2, 4)
 
     __author__  = "Domingo E. Savoretti"
 
