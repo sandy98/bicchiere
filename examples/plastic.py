fa1ec2b5ff6b2a5d41b816c0d59ed32c026cf358
sandy98 <esavoretti@gmail.com>
2022-08-20 02:20:33 +0000

    Revamping session mechanisms

--- examples/plastic.py
+++ examples/plastic.py
@@ -1,27 +1,38 @@
 # -*- coding: utf-8 -*-
 
-class Chameleon(dict):
-    def __init__(self, **kw):
+from uuid import uuid4
+import json
+
+class Session(dict):
+    def __init__(self, sid = uuid4().hex, **kw):
+        self.sid = sid
         self.update(**kw)
 
-    def __getattribute__(self, __name: str):
+    def __getattr__(self, __name: str):
         if __name in self:
             return self[__name]
-        elif __name == "save":
-            return object.__getattribute__(self, "save")
-        elif hasattr(dict, __name):
-            return dict.__getattribute__(self, __name)
         else:
-            raise AttributeError(f"{self.__class__.__name__} object has no attribute '{__name}'")
+            raise AttributeError(f"getattr informs that {self.__class__.__name__} object has no attribute '{__name}'")
+
+    def __getattribute__(self, __name: str):
+        return super().__getattribute__(__name)
+
+    def __setitem__(self, __k: str, __v) -> None:
+        super().__setitem__(__k, __v)
+        self.save()
 
     def __setattr__(self, __name: str, __value) -> None:
-        self[__name] = __value
+        self.__setitem__(__name, __value)
 
     def __delattr__(self, __name: str) -> None:
         #return super().__delattr__(__name)
         if self.get(__name):
             del self[__name]
+            self.save()
 
-    def save(self):
-        print(f"Saving {dict(self)}")
-
+    def save(self) -> str:
+        d = dict()
+        d[self.sid] = self
+        j = json.dumps(d)
+        print(f"Saving {d}")
+        return j
