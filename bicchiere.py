cef93de1440bacae10c320c6a7a74a63545c2d1e
sandy98 <esavoretti@gmail.com>
2022-08-31 14:47:59 -0300

    version 0.6.8 Erased pestering print statements

--- bicchiere.py
+++ bicchiere.py
@@ -767,7 +767,7 @@ default_config = SuperDict({
 class BicchiereMiddleware:
     "Base class for everything Bicchiere"
 
-    __version__ = (0, 6, 7)
+    __version__ = (0, 6, 8)
     __author__ = "Domingo E. Savoretti"
     config = default_config
     template_filters = {}
