48cde7610baeb42c34f2ab3b80b2f5eb50fe31be
sandy98 <esavoretti@gmail.com>
2022-10-10 16:25:03 -0300

    version 1.6.1 CGI handling finished and working smoothly.

--- bicchiere.py
+++ bicchiere.py
@@ -1560,7 +1560,7 @@ default_config = SuperDict({
 class BicchiereMiddleware:
     "Base class for everything Bicchiere"
 
-    __version__ = (1, 6, 0)
+    __version__ = (1, 6, 1)
     __author__ = "Domingo E. Savoretti"
     config = default_config
     template_filters = {}
